068909a7c7bd80e55dd70ecf538a661617d0a1ff
Muhammet Cin <muhammetacin@gmail.com>
2022-01-24 12:54:03 +0100

    Fetch city when pressed enter

--- js/weather.js
+++ js/weather.js
@@ -1,12 +1,8 @@
 const cityName = document.getElementById('inputField');
 const submitBtn = document.getElementById('submit');
 
-// TODO: import weather api
 const APIkey = "c863e7658bfbe95e2049298afb62ccf5";
 const apiString = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName.value + "&appid=" + APIkey;
-console.log('api.openweathermap.org/data/2.5/weather?q=London&appid=' + APIkey);
-
-
 
 async function getCity(cityName) {
     const apiString = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIkey;
@@ -20,7 +16,7 @@ async function getCity(cityName) {
 
 const showResult = (event) => {
     if(event.key == "Enter") {
-        console.log(cityName.value.toLowerCase());
+        getCity(cityName.value);
     }
 }
 
