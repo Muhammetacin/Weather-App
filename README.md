05df3b0aedac91a7d6932731b910d5fdec37c1f0
Muhammet Cin <muhammetacin@gmail.com>
2022-01-24 13:09:07 +0100

    Solve APIkey exposure

--- README.md
+++ README.md
@@ -25,7 +25,6 @@ Enough is enough, you decide to build a small web application for them so that y
 
 - In the home page the user can enter the city of his/her choice (think of the right HTML elements here)
   - Add input field for user to type city name and grab its value in js
-  - Show all cities that match in a list
 - On clicking the SUBMIT button or pressing ENTER the application will display the weather for the next 5 days
   - Add click event on submit button or keyup on enter button so search for city
   - If there's no match show a message 'no cities found'
--- js/weather.js
+++ js/weather.js
@@ -1,16 +1,14 @@
+import { APIkey } from './config.js';
+
 const cityName = document.getElementById('inputField');
 const submitBtn = document.getElementById('submit');
 
-const APIkey = "c863e7658bfbe95e2049298afb62ccf5";
-const apiString = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName.value + "&appid=" + APIkey;
-
 async function getCity(cityName) {
     const apiString = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIkey;
-    try {
-      const response = await fetch(apiString).then(response => response.json());
-      console.log(response);
-    } catch (error) {
-      console.error(error);
+    const response = await fetch(apiString).then(response => response.json());
+    console.log(response);
+    if(response.cod == 404) {
+        console.log(response.message.toUpperCase());
     }
 }
 
