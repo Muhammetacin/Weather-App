eeb90ba0844667be7e3a5408d0c7889a7b6d99f8
Muhammet Cin <muhammetacin@gmail.com>
2022-01-24 12:53:14 +0100

    Fetch city information by typing name

--- js/weather.js
+++ js/weather.js
@@ -1,2 +1,30 @@
+const cityName = document.getElementById('inputField');
+const submitBtn = document.getElementById('submit');
+
 // TODO: import weather api
-const apiString = "api.openweathermap.org/data/2.5/weather?q=" + {cityName} + "&appid=" + {APIkey};
+const APIkey = "c863e7658bfbe95e2049298afb62ccf5";
+const apiString = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName.value + "&appid=" + APIkey;
+console.log('api.openweathermap.org/data/2.5/weather?q=London&appid=' + APIkey);
+
+
+
+async function getCity(cityName) {
+    const apiString = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIkey;
+    try {
+      const response = await fetch(apiString).then(response => response.json());
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+}
+
+const showResult = (event) => {
+    if(event.key == "Enter") {
+        console.log(cityName.value.toLowerCase());
+    }
+}
+
+cityName.addEventListener('keyup', showResult);
+submitBtn.addEventListener('click', (event) => {
+    getCity(cityName.value);
+})
