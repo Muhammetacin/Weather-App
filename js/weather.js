d19f803f804c78e930b5c3c53ef90d9854d74881
Muhammet Cin <muhammetacin@gmail.com>
2022-01-25 13:51:32 +0100

    Temp and status for all days works. Day names WIP

--- js/weather.js
+++ js/weather.js
@@ -7,19 +7,32 @@ const cityProperties = document.getElementById('cityProperties');
 const showCityName = document.getElementById('inputArea').children[0];
 
 async function getCity5Days(cityName) {
-    const apiString5Days = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&cnt=7&units=metric&appid=" + APIkey;
+    const apiString5Days = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&cnt=40&units=metric&appid=" + APIkey;
     const response5Days = await fetch(apiString5Days).then(response => response.json());
 
-    // Contains city name, temp day 1-2-3-4-5-6
-    const responseValues = [
+    if(response5Days.city == undefined) {
+        
+        return;
+    }
+
+    const skyData = [
+        response5Days.list[0].weather[0].main,
+        response5Days.list[8].weather[0].main,
+        response5Days.list[16].weather[0].main,
+        response5Days.list[24].weather[0].main,
+        response5Days.list[32].weather[0].main,
+        response5Days.list[39].weather[0].main,
+    ];
+
+    // // Contains city name, temp day 1-2-3-4-5-6-7
+    const responseTempValues = [
         response5Days.city.name,
         response5Days.list[0].main.temp,
-        response5Days.list[1].main.temp,
-        response5Days.list[2].main.temp,
-        response5Days.list[3].main.temp,
-        response5Days.list[4].main.temp,
-        response5Days.list[5].main.temp,
-        response5Days.list[6].main.temp,
+        response5Days.list[8].main.temp,
+        response5Days.list[16].main.temp,
+        response5Days.list[24].main.temp,
+        response5Days.list[32].main.temp,
+        response5Days.list[39].main.temp,
     ];
 
     // Get today's date
@@ -30,18 +43,13 @@ async function getCity5Days(cityName) {
     console.log(response5Days);
 
     // Show city name on top
-    showCityName.textContent = responseValues[0];
+    showCityName.textContent = responseTempValues[0];
 
-    // Show todays temperature
-    setTodayTemp(responseValues[1]);
+    // Show temperature for all days
+    setTempAllDays(responseTempValues);
 
-    // cityProperties.children[1].textContent = responseValues[1];
-    // cityProperties.children[2].children[1].textContent = responseValues[2];
-    // cityProperties.children[2].children[3].textContent = responseValues[3];
-    // cityProperties.children[2].children[5].textContent = responseValues[4];
-    // cityProperties.children[2].children[7].textContent = responseValues[5];
-    // cityProperties.children[2].children[9].textContent = responseValues[6];
-    // cityProperties.children[2].children[11].textContent = responseValues[7];
+    // Show sky description for all days
+    setSkyStatusAllDays(skyData);
 }
 
 function capitalizeFirstLetter(string) {
@@ -64,18 +72,38 @@ submitBtn.addEventListener('click', () => {
 });
 
 
-const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-let todaysdate = new Date();
 
-console.log("Today is", daysOfWeek[todaysdate.getDay()]);
-console.log("Next day is", daysOfWeek[todaysdate.getDay() + 1]);
-console.log("Next day is", daysOfWeek[todaysdate.getDay() + 2]);
+const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const dayNamesOfWeek = document.getElementById('daysOfWeek');
 
 function getDayAndSetAllDaysOfWeek(today) {
-    console.log("Today is", daysOfWeek[today.getDay()]);
+    // console.log("Today is", daysOfWeek[today.getDay()]);
 
+    for(let i = 0; i < dayNamesOfWeek.children.length; i++) {
+        console.log(dayNamesOfWeek.children.length);
+        console.log((i + today.getDay() + 1) );
+        if((i + today.getDay() + 1) >= daysOfWeek.length) {
+            dayNamesOfWeek.children[i].children[0].innerHTML = daysOfWeek[0];
+            console.log(dayNamesOfWeek.children[i].children[0].innerHTML = daysOfWeek[0]);
+        } 
+        else {
+            dayNamesOfWeek.children[i].children[0].innerHTML = daysOfWeek[i + 1 + today.getDay() + 1];
+        }
+    }
 }
 
-function setTodayTemp(todaysTemp) {
-    cityProperties.children[1].innerHTML = Math.round(todaysTemp) + "°";
+function setTempAllDays(tempData) {
+    cityProperties.children[2].innerHTML = Math.round(tempData[1]) + "°";
+    
+    for(let i = 0; i < dayNamesOfWeek.children.length; i++) {
+        dayNamesOfWeek.children[i].children[2].innerHTML = Math.round(tempData[i + 2]) + "°";
+    }
+}
+
+function setSkyStatusAllDays(skyData) {
+    cityProperties.children[1].innerHTML = skyData[0];
+
+    for(let i = 0; i < dayNamesOfWeek.children.length; i++) {
+        dayNamesOfWeek.children[i].children[1].innerHTML = skyData[i + 1];
+    }
 }
